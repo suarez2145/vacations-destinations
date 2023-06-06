515f60105746575036ce3f5ba9a0c6ca9ecf0908
exxon suarez <suarez.exxon@gmail.com>
2023-06-06 15:29:29 -0700

    changed src directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: path.resolve(__dirname, 'docs'),
   publicDir: 'public',
   resolve: {
     alias: {
